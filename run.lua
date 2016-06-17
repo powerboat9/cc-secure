102d1ac3c11db60e3dd43d0a93f8c2dd3827d17e
powerboat9 <powerboat9@users.noreply.github.com>
2016-06-17 08:03:41 -0400

    Update run.lua

--- run.lua
+++ run.lua
@@ -1,6 +1,14 @@
+env = {}
+
 do
     local me = shell.getRunningProgram()
     local meDir = me:sub(1, 0 - #fs.getName(me))
     os.loadAPI(meDir .. "table-copy")
+    if not table-copy then error("API not found", 0) end
+    table.copy(_G, env)
+    os.unloadAPI(meDir .. "table-copy")
 end
 
+local function prompt()
+
+function env.elevate(
